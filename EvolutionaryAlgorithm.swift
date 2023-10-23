a0ebb4a1fb878b5f3ed8717c9d7b0abdc55a6a74
Ananta Shahane <ananta.shahane@aol.com>
2023-10-23 14:08:17 +0200

    First Optimisation.

--- EvolutionaryAlgorithm.swift
+++ EvolutionaryAlgorithm.swift
@@ -18,7 +18,7 @@ struct Individual {
         mutationAmplitude = [Int: Double]()
         mutationDirection = [Int : Double]()
         for i in 1...numberofSignals {
-            weightage[i] = Double.random(in: 0...1)
+            weightage[i] = abs(Double.NormalRandom(mu: 10, sigma: 10))
             mutationAmplitude[i] = 1
             mutationDirection[i] = 0
         }
@@ -33,16 +33,18 @@ class EvolutionaryAlgorithm {
     let parentPopulationSize : Int
     
     //Variables
+    var bestError = Double.infinity
     var parentPopulation = [Individual]()
     var offspringPopulation = [Individual]()
     var recombinedEntity : Individual
     init(parentPopulationSize : Int, offspringPopulationSize : Int) {
         baseTimeSeries = [:]
         var weightage = [Int : Double]()
-        for i in 1...5 {
+        for i in 1...10 {
             baseTimeSeries[i] = GenerateData(amplitude: Double.random(in: 1...10), frequency: Double.random(in: 1...20), phase: Double.random(in: 1...10_000))
             weightage[i] = Double.random(in: 1...100)
         }
+        print("Original weightage: \(weightage)")
         data = MixTimeSeries(timeSeries: baseTimeSeries, weightage: weightage)
         recombinedEntity = Individual(numberofSignals: baseTimeSeries.count)
         subtimeSeriesCount = 3
@@ -65,7 +67,7 @@ class EvolutionaryAlgorithm {
                 for (index, _) in timeSeries.enumerated() {
                     error += abs(timeSeries[index] - data[index])
                 }
-                parentPopulation[parentNumber].error = error / Double(timeSeries.count)
+                parentPopulation[parentNumber].error = error / abs(data.reduce(0, +))
             }
         } else {
             for (offspringNumber, offspring) in offspringPopulation.enumerated() {
@@ -74,7 +76,7 @@ class EvolutionaryAlgorithm {
                 for (index, _) in timeSeries.enumerated() {
                     error += abs(timeSeries[index] - data[index])
                 }
-                offspringPopulation[offspringNumber].error = error / Double(timeSeries.count)
+                offspringPopulation[offspringNumber].error = error / abs(data.reduce(0, +))
             }
             let timeSeries = MixTimeSeries(timeSeries: baseTimeSeries, weightage: recombinedEntity.weightage)
             var error = 0.0
@@ -97,14 +99,16 @@ class EvolutionaryAlgorithm {
         var returnIndividual = recombinedEntity
         // Mutate Amplitude
         for key in returnIndividual.weightage.keys {
-//            if Int.random(in: 1...100) > 10 {
-//                returnIndividual.mutationDirection[key] = 0
-//                returnIndividual.mutationAmplitude[key] = 1
-//            }
-            returnIndividual.mutationDirection[key] = Double.NormalRandom(mu: returnIndividual.mutationDirection[key] ?? 0, sigma: 1)
-            returnIndividual.mutationAmplitude[key]! *= pow(2.71, returnIndividual.mutationDirection[key] ?? 0)
-            let mutation = returnIndividual.weightage[key]! * returnIndividual.mutationAmplitude[key]!
-            returnIndividual.weightage[key] = mutation
+            if Int.random(in: 1...100) > 5 {
+                returnIndividual.mutationDirection[key] = 0
+                returnIndividual.mutationAmplitude[key] = 1
+            }
+            if Int.random(in: 1...100) > 50 {
+                returnIndividual.mutationDirection[key] = Double.NormalRandom(mu: returnIndividual.mutationDirection[key] ?? 0, sigma: bestError)
+                returnIndividual.mutationAmplitude[key]! *= pow(2.71, returnIndividual.mutationDirection[key] ?? 0)
+                let mutation = returnIndividual.weightage[key]! * returnIndividual.mutationAmplitude[key]!
+                returnIndividual.weightage[key] = mutation
+            }
         }
         return returnIndividual
     }
@@ -128,13 +132,16 @@ class EvolutionaryAlgorithm {
         Initialise()
         var bestSolution  = Individual(numberofSignals: 3)
         Evaluate(evaluateParentPopulation: true)
-        for iteration in 1...50000 {
+        for iteration in 1...10000 {
             Recombine()
             Mutate()
             Evaluate(evaluateParentPopulation: false)
             Selection()
             if let bstSolution = parentPopulation.sorted(by: {$0.error ?? Double.infinity > $1.error ?? Double.infinity}).first {
                 bestSolution = bstSolution
+                if bestError > bestSolution.error! {
+                    bestError = bestSolution.error!
+                }
                 print("Iteration \(iteration): Min Loss \(bestSolution.error!), best fit \(bestSolution.weightage.sorted(by: {$0.key < $1.key}).map({$0.value})).")
             }
         }
